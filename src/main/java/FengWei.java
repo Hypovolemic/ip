d59d90f8e53765d8d0604b01eb1483787be0bfdc
Hypovolemic <weifengwong1018@gmail.com>
2025-08-21 23:48:06 +0800

    Level-1

--- src/main/java/FengWei.java
+++ src/main/java/FengWei.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class FengWei {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("_____________________________________");
         System.out.println("Hello! I'm FengWei");
         System.out.println("What can I do for you?");
         System.out.println("_____________________________________");
+
+        // Echo commands entered by the user, and exits when the user types the command bye
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+
+            System.out.println("_____________________________________");
+            System.out.println(input);
+            System.out.println("_____________________________________");
+        }
+
+        System.out.println("_____________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("_____________________________________");
+        scanner.close();
     }
 }
