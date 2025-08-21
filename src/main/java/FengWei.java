186abe7a952a3d29838c0a73a1bdc8fd425700cd
Hypovolemic <weifengwong1018@gmail.com>
2025-08-21 23:26:40 +0800

    Level-0

---
+++ src/main/java/FengWei.java
@@ -0,0 +1,10 @@
+public class FengWei {
+    public static void main(String[] args) {
+        System.out.println("_____________________________________");
+        System.out.println("Hello! I'm FengWei");
+        System.out.println("What can I do for you?");
+        System.out.println("_____________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("_____________________________________");
+    }
+}
